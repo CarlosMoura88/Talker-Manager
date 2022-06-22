490a8e69e145a0aa5f450d3ca1aceeb42bfc922a
Carlos Moura <carlosmourapires88@gmail.com>
2022-06-22 18:38:39 -0300

    Requisito 2 concluído

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,14 +8,30 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const talkerJSON = async () => JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+
 // Requisito 1
 
 app.get('/talker', async (_req, res) => {
-  const talkers = await fs.readFile('./talker.json', 'utf-8');
-  if (talkers.length > 0) {
-    res.status(HTTP_OK_STATUS).send(JSON.parse(talkers));
+  const allTalkers = await talkerJSON();
+
+  if (allTalkers.length > 0) {
+    res.status(HTTP_OK_STATUS).send(allTalkers);
   } else {
     res.status(HTTP_OK_STATUS).send([]);
+  }
+});
+
+// Requisito 2
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;  
+  const talkers = await talkerJSON();
+  const talker = talkers.find((element) => element.id === Number(id));
+  if (talker) {
+    res.status(HTTP_OK_STATUS).send(talker);
+  } else {
+    res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
   }
 });
 
